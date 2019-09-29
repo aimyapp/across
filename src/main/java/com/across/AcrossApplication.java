f1612e6115e44a661fed041be0d73570aa05bd47
aimy <aimy.app@gmail.com>
2019-09-29 14:42:47 +0900

    first commit

---
+++ src/main/java/com/across/AcrossApplication.java
@@ -0,0 +1,13 @@
+package com.across;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcrossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcrossApplication.class, args);
+	}
+
+}
